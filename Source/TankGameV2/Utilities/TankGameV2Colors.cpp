1d12d1d95f8fa21e6227147cc689f32a489782c3
Nicholas Jordan <nijordan99@gmail.com>
2021-07-17 15:38:05 -0700

    Added HUD elements to the new Color class
    Added HUD elements to the new Color class

--- Source/TankGameV2/Utilities/TankGameV2Colors.cpp
+++ Source/TankGameV2/Utilities/TankGameV2Colors.cpp
@@ -3,14 +3,68 @@
 
 #include "TankGameV2Colors.h"
 
+//DamageColor
 void UTankGameV2Colors::SetDamageColor(FSlateColor NewDamageColor)
 {
-	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Damage"));
 	DamageColor = NewDamageColor;
 }
 
+FLinearColor UTankGameV2Colors::GetDamageColor()
+{
+	return DamageColor.GetSpecifiedColor();
+}
+
+//HealColor
 void UTankGameV2Colors::SetHealColor(FSlateColor NewHealColor)
 {
-	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Heal"));
 	HealColor = NewHealColor;
+}
+
+FLinearColor UTankGameV2Colors::GetHealColor()
+{
+	return HealColor.GetSpecifiedColor();
+}
+
+//ReloadBar
+void UTankGameV2Colors::SetReloadBarColor(FSlateColor NewReloadBarColor)
+{
+	ReloadBarColor = NewReloadBarColor;
+}
+
+FLinearColor UTankGameV2Colors::GetReloadBarColor()
+{
+	return ReloadBarColor.GetSpecifiedColor();
+}
+
+//ReloadBar Text
+void UTankGameV2Colors::SetReloadBarTextColor(FSlateColor NewReloadBarTextColor)
+{
+	ReloadBarTextColor = NewReloadBarTextColor;
+}
+
+FLinearColor UTankGameV2Colors::GetReloadBarTextColor()
+{
+	return ReloadBarTextColor.GetSpecifiedColor();
+}
+
+//HealthBar
+void UTankGameV2Colors::SetHealthBarColor(FSlateColor NewHealthBarColor)
+{
+	HealthBarColor = NewHealthBarColor;
+}
+
+FLinearColor UTankGameV2Colors::GetHealthBarColor()
+{
+	return HealthBarColor.GetSpecifiedColor();
+}
+
+//HealthBar Text
+void UTankGameV2Colors::SetHealthBarTextColor(FSlateColor NewHealthBarTextColor)
+{
+	HealthBarTextColor = NewHealthBarTextColor;
+}
+
+FLinearColor UTankGameV2Colors::GetHealthBarTextColor()
+{
+	return HealthBarTextColor.GetSpecifiedColor();
 }
--- Source/TankGameV2/Utilities/TankGameV2Colors.h
+++ Source/TankGameV2/Utilities/TankGameV2Colors.h
@@ -18,12 +18,43 @@ public:
 
 	//Uninitialized Slate colors are Fuchsia by default
 	//Initial Hex string from UE4 color picker sRGB Hex field
-	static inline FSlateColor DamageColor = FSlateColor(FColor::FromHex(FString(TEXT("FFFF60FF"))));	//pale yellow
-	static inline FSlateColor HealColor = FSlateColor(FColor::FromHex(FString(TEXT("536FFFFF"))));		//pale blue
+	static inline FSlateColor DamageColor = FSlateColor(FColor::FromHex(FString(TEXT("FFFF60FF"))));		//pale yellow
+	static inline FSlateColor HealColor = FSlateColor(FColor::FromHex(FString(TEXT("536FFFFF"))));			//pale purpley blue
 
+	static inline FSlateColor ReloadBarColor = FSlateColor(FColor::FromHex(FString(TEXT("89CFFFFF"))));		//very pale blue
+	static inline FSlateColor ReloadBarTextColor = ReloadBarColor;
+	static inline FSlateColor HealthBarColor = FSlateColor(FColor::FromHex(FString(TEXT("DA1C00FF"))));		//red
+	static inline FSlateColor HealthBarTextColor = FSlateColor(FColor::FromHex(FString(TEXT("FFFFFFFF"))));	//white
+
+	//Getters and setters are for blueprint access
+public:
 	UFUNCTION(BlueprintCallable, Category = "TankGameV2 Color")
 		static void SetDamageColor(FSlateColor NewDamageColor);
+	UFUNCTION(BlueprintCallable, Category = "TankGameV2 Color")
+		static FLinearColor GetDamageColor();
 
 	UFUNCTION(BlueprintCallable, Category = "TankGameV2 Color")
 		static void SetHealColor(FSlateColor NewHealColor);
+	UFUNCTION(BlueprintCallable, Category = "TankGameV2 Color")
+		static FLinearColor GetHealColor();
+
+	UFUNCTION(BlueprintCallable, Category = "TankGameV2 Color")
+		static void SetReloadBarColor(FSlateColor NewReloadBarColor);
+	UFUNCTION(BlueprintCallable, Category = "TankGameV2 Color")
+		static FLinearColor GetReloadBarColor();
+
+	UFUNCTION(BlueprintCallable, Category = "TankGameV2 Color")
+		static void SetReloadBarTextColor(FSlateColor NewReloadBarTextColor);
+	UFUNCTION(BlueprintCallable, Category = "TankGameV2 Color")
+		static FLinearColor GetReloadBarTextColor();
+
+	UFUNCTION(BlueprintCallable, Category = "TankGameV2 Color")
+		static void SetHealthBarColor(FSlateColor NewHealthBarColor);
+	UFUNCTION(BlueprintCallable, Category = "TankGameV2 Color")
+		static FLinearColor GetHealthBarColor();
+
+	UFUNCTION(BlueprintCallable, Category = "TankGameV2 Color")
+		static void SetHealthBarTextColor(FSlateColor NewHealthBarTextColor);
+	UFUNCTION(BlueprintCallable, Category = "TankGameV2 Color")
+		static FLinearColor GetHealthBarTextColor();
 };
